3b313029786b42b427ba2a34704af41595643d58
jp05451 <jp05451@yahoo.com.tw>
2022-03-28 22:38:49 +0800

    first commit

---
+++ SelectionSort.h
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+void selectionSort(T A[], size_t size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        T min = 0;
+        for (int i = 0; i < size;i++)
+        {
+            if(A[i]> -10e9)
+            {
+                min = A[i];
+            }
+        }
+        int pos;
+        for (int j = i; j < size; j++)
+        {
+            if (A[j] < min)
+            {
+                min = A[j];
+                pos = j;
+            }
+        }
+        swap(A[i], A[pos]);
+    }
+}
